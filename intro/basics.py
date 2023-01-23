42d0f35e804393964d6164a1e9468e968c3f8104
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-23 07:32:02 +0300

    tuples

--- intro/basics.py
+++ intro/basics.py
@@ -1,4 +1,7 @@
 # Introduction to python data structures.
 # Variables-- multiple assignment
 a, b, c = 5, 6, 7
-print(a, b, c)
+# print(a, b, c)
+# tuple
+my_data = (1, "Denzel", (11, 12, 13))
+print(my_data[1][2])
