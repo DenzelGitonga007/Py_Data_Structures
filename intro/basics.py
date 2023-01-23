e6550a6f27f4d71285d47163a082f372e3f43db0
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-23 07:42:58 +0300

    dictionaries

--- intro/basics.py
+++ intro/basics.py
@@ -2,9 +2,14 @@
 # Variables-- multiple assignment
 a, b, c = 5, 6, 7
 # print(a, b, c)
+
 # tuple
 my_data = (1, "Denzel", [11, 12, 13])
 # print(my_data[1][2])
 # Changing the mutable items inside the tuple
 my_data[2][1] = 20
-print(my_data)
+# print(my_data)
+
+# Dictionaries
+a = {1:"Hi", 2:"Denzel", "age":21}
+print(a[1], a["age"])
