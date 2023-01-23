37ec3c2fe50ddbce4fed266b2605a293f0c11325
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-23 07:41:03 +0300

    learning

--- intro/basics.py
+++ intro/basics.py
@@ -3,5 +3,8 @@
 a, b, c = 5, 6, 7
 # print(a, b, c)
 # tuple
-my_data = (1, "Denzel", (11, 12, 13))
-print(my_data[1][2])
+my_data = (1, "Denzel", [11, 12, 13])
+# print(my_data[1][2])
+# Changing the mutable items inside the tuple
+my_data[2][1] = 20
+print(my_data)
