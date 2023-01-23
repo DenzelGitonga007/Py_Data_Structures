a9d2b2bc50122b833805b5d074713e94056aba70
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-23 07:56:54 +0300

    lists as stacks

---
+++ intro/data_structures.py
@@ -0,0 +1,8 @@
+# List as stacks
+# The last element in the list, is the first to be removed in the stack
+stack = [3, 4, 5]
+# Add a last item to the stack/list
+stack.append(7)
+# Pop out the last item
+# stack.pop()
+print(stack.pop())
