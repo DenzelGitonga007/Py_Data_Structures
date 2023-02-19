100a28682ab2360bdcb9737365b7afd77e5179eb
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-19 20:33:31 +0300

    created db

--- Working_With_Db/sqlite_trial.py
+++ Working_With_Db/sqlite_trial.py
@@ -1,1 +1,8 @@
 # Using sqlite with python
+import sqlite3
+
+
+#Create or connect to the database
+sqlite_conn = sqlite3.connect('MyHospital.db')
+
+print ("Database connected") # confirm that the previous line, which creates the DB, is actually ran
