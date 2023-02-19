086be75b64132db58bf24373ba69fcfd4fc0b4a9
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-19 20:44:12 +0300

    run Sql, and check version

--- Working_With_Db/sqlite_trial.py
+++ Working_With_Db/sqlite_trial.py
@@ -6,3 +6,13 @@ import sqlite3
 sqlite_conn = sqlite3.connect('MyHospital.db')
 
 print ("Database connected") # confirm that the previous line, which creates the DB, is actually ran
+
+# To execute SQLite statements in Python, you need a cursor object
+#Create a cursor object
+conn = sqlite_conn.cursor()
+
+# Print sqlite version
+dbversion = conn.execute("select sqlite_version();")
+print("\nSQLite Database Version is: ", dbversion.fetchall())
+
+
